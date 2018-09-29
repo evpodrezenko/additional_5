a7d58e316b28970cdbd422623853a6dab96358d7
evpodrezenko <evpodrezenko@gmail.com>
2018-09-29 15:46:06 +0300

    Finding the correct bracket sequence using the stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsMap = new Map(bracketsConfig);
+  const stack = [];
+
+  for (let i = 0, len = str.length; i < len; i++) {
+    let bracket = str[i];
+    const lastBracket = stack[stack.length - 1];
+    const closingBracket = bracketsMap.get(lastBracket);
+    const isClosingBracket = (bracket === closingBracket);
+ 
+    if (isClosingBracket) {
+      stack.pop();
+    } else {
+      stack.push(bracket);
+    }
+  }
+  
+  return (stack.length === 0);
 }
